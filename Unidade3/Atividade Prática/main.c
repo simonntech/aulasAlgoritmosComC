e613a70e8d14846562bbc727f18da0c4f4ac5907
Simon Tech <contato.simon.tech@gmail.com>
2025-09-25 11:29:49 -0300

    final

--- Unidade3/Atividade Prática/main.c
+++ Unidade3/Atividade Prática/main.c
@@ -3,10 +3,25 @@
 
 int main(){
     int vendas[QNT_VENDAS];
+    int vendaTotal=0;
 
+    printf("Dados de vendas\n\n");
+    printf("-------------------\n\n");
     for(int i=0; i< QNT_VENDAS; i++){
-
+        int numerador = i+1;
+        printf("Quantidade de vendas do %io dia: ", numerador);
+        scanf("%i", &vendas[i]);
     };
+
+    printf("-------------------\n\n");
+    for(int j=0; j< QNT_VENDAS; j++){
+        int numerador = j+1;
+        vendaTotal += vendas[j];
+        printf("Vendas no %i dia: %i\n", numerador, vendas[j]);
+    }
+
+    printf("---------------------\n\n");
+    printf("Total de vendas no periodo de %i dias: %i",QNT_VENDAS,vendaTotal );
 
     return 0;
 }
