cf697641cd524f2f9743b8fac44a6c273563107c
Simon Tech <contato.simon.tech@gmail.com>
2025-09-29 14:58:58 -0300

    functions

--- Unidade4/Atividade Prática/main.c
+++ Unidade4/Atividade Prática/main.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+float calcular_salario_bruto(float valor_hora, int qnt_horas){
+    float salario_bruto = valor_hora * qnt_horas;
+    return salario_bruto;
+}
+
+float calcular_desconto(float salario_bruto){
+    float desconto = salario_bruto - (salario_bruto * 0.09);
+    return desconto;
+}
+
+float calcular_salario_liquido(float salario_bruto, float desconto){
+    float salario_liquido = salario_bruto - desconto;
+    return salario_liquido;
+}
